ac1581e6755429ed6ead668dbfd81f02b3576e8c
rem0g <gotterspeer@gmail.com>
2016-07-11 19:23:12 +0200

    tes

--- firstClass.py
+++ firstClass.py
@@ -1,1 +1,20 @@
-print("Dit is een test")
+import random
+
+class Hond:
+    brokjes = ["Snoep", "Koekje", "Cake", "Pizza"]
+
+    def __init__(self):
+        self.Dog = "Hondje"
+
+    def blaffen(self):
+        return self.Dog + " blaft"
+
+    def kiesEten(self):
+        return random.choice(self.brokjes)
+
+    def gaRekenen(self, eerstGetal, tweedeGetal):
+        return "Woef woef " + str(eerstGetal * tweedeGetal)
+
+
+
+
