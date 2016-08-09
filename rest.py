4fa7aa27c0cdac6e561f2cf9421fbc7bf093c3dc
rem0g <gotterspeer@gmail.com>
2016-08-09 15:31:53 +0200

    Merge remote-tracking branch 'remotes/origin/Hugo' into Gomer
    # Conflicts:
    #	firstClass.py

--- rest.py
+++ rest.py
@@ -1,24 +1,32 @@
 import pycurl
 from io import BytesIO
-import certifi
 import sys
 import json
 
 
-params = {
-  'type': '123',
-  'limit': '123',
-    'start': '0',
-    'access_token': '123'
-}
+params = json.dumps([
+  'type : 123',
+  'limit : 123',
+    'start : 0',
+    'access_token : 123'
+])
+http_header = [
+    'Content-Type: application/json',
+    'Content-Length: ' + str(sys.getsizeof(params))
+]
 
 url = 'https://api.copernica.com/emailings/'
 
 b = BytesIO()
 c = pycurl.Curl()
 
-c.setopt(pycurl.CAINFO, certifi.where())
 c.setopt(pycurl.URL, url)
 c.setopt(pycurl.WRITEDATA, b)
+c.setopt(pycurl.HEADER, 1)
+c.setopt(pycurl.CUSTOMREQUEST, "POST")
+c.setopt(pycurl.POST, 1)
+c.setopt(pycurl.POSTFIELDS, params)
+c.setopt(pycurl.HTTPHEADER, http_header)
+
 c.perform()
 print(b.getvalue())
