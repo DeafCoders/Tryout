c8061dccd67cd0f1c623fab32146409ca503738f
rem0g <gotterspeer@gmail.com>
2016-08-09 21:37:46 +0200

    Merge remote-tracking branch 'remotes/origin/Hugo' into Gomer
    # Conflicts:
    #	firstClass.py

--- rest.py
+++ rest.py
@@ -9,7 +9,10 @@ import urllib.parse as urllib
 class Copernica:
 
     def __init__(self):
-       leeg = 0
+       params = {}
+       params['limit'] = 1  # limiet standaard 9999, geen beperking
+       params['start'] = 0  # start altijd vanuit 0
+       self.params = params
 
     def curl_options(self, url):
         b = BytesIO()
@@ -25,22 +28,23 @@ class Copernica:
 
     def get_mailings(self, token):
         self.token = token
-        params = {}
-        params['type'] = 'mass'  # Type te kiezen uit bulk of mass
-        params['limit'] = 1  # limiet standaard 9999, geen beperking
-        params['start'] = 0  # start altijd vanuit 0
-        params['access_token'] = token
-        return self.curl_options("https://api.copernica.com/emailings/?" + urllib.urlencode(params))
+        self.params['access_token'] = token
+        self.params['type'] = 'mass'  # Type te kiezen uit bulk of mass
+        return self.curl_options("https://api.copernica.com/emailings/?" + urllib.urlencode(self.params))
 
-    def get_mailing_details(self, token, emailing_id):
-        params = {}
-        params['access_token'] = token
-        return self.curl_options("https://api.copernica.com/emailing/" + str(emailing_id) + '/statistics/?' + urllib.urlencode(params))
+    def get_mailing_details(self, token, emailing_id, details):
 
+        details_url = {
+            'statistics': '/statistics/?',
+            'deliveries': '/deliveries/?',
+            'destinations': '/destinations/?'
+        }
 
+        self.params['access_token'] = token
+        return self.curl_options("https://api.copernica.com/emailing/" + str(emailing_id) + details_url[details] + urllib.urlencode(self.params))
 
 
 #Ophalen alle mailings ahv token van een klant
 print(Copernica().get_mailings("5fad6ce4eeca28c7fbd41f16073dc2e4f67f823b925d40ea3fc6bd8eb313b5723fdc7a0af0d65a01c52fefeaa35beec951e3ff4fe905d0a12a1342f5b070a313"))
 #ophalen statistieken ahv token van een klant
-print(Copernica().get_mailing_statistics("5fad6ce4eeca28c7fbd41f16073dc2e4f67f823b925d40ea3fc6bd8eb313b5723fdc7a0af0d65a01c52fefeaa35beec951e3ff4fe905d0a12a1342f5b070a313", "19"))
+print(Copernica().get_mailing_details("5fad6ce4eeca28c7fbd41f16073dc2e4f67f823b925d40ea3fc6bd8eb313b5723fdc7a0af0d65a01c52fefeaa35beec951e3ff4fe905d0a12a1342f5b070a313", "19", "deliveries"))
