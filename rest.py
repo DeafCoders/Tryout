7493cd8a58c0bde640f99e1092304028fd2b9f43
rem0g <gotterspeer@gmail.com>
2016-08-09 21:26:58 +0200

    Merge remote-tracking branch 'remotes/origin/Hugo' into Gomer
    # Conflicts:
    #	firstClass.py

--- rest.py
+++ rest.py
@@ -4,56 +4,43 @@ import sys
 import json
 import urllib.parse as urllib
 
-params = {}
-params['type'] = 'mass' #Type te kiezen uit bulk of mass
-params['limit'] = 1 #limiet standaard 9999, geen beperking
-params['start'] = 0 #start altijd vanuit 0
-params['access_token'] = "5fad6ce4eeca28c7fbd41f16073dc2e4f67f823b925d40ea3fc6bd8eb313b5723fdc7a0af0d65a01c52fefeaa35beec951e3ff4fe905d0a12a1342f5b070a313"
-#toegangstoken, uniek per gebruiker. Deze is van E-mailmarketingbureau Ace and Tate
 
-http_header = [
-    'Content-Type: application/json'
-]
+#Copernica API Class
+class Copernica:
 
-url = "https://api.copernica.com/emailings/?" + urllib.urlencode(params)
-print(url)
-b = BytesIO()
-c = pycurl.Curl()
+    def __init__(self):
+       leeg = 0
 
-c.setopt(pycurl.URL, url)
-c.setopt(pycurl.VERBOSE, 1)
-c.setopt(pycurl.WRITEDATA, b)
+    def curl_options(self, url):
+        b = BytesIO()
+        c = pycurl.Curl()
 
-c.perform()
-output_values = json.loads(b.getvalue().decode())
-print(type(output_values))
-print(output_values)
+        c.setopt(pycurl.URL, url)
+        c.setopt(pycurl.VERBOSE, 0) #1 = debug aan, 0 = debug uit
+        c.setopt(pycurl.WRITEDATA, b)
 
+        c.perform()
+        output_values = json.loads(b.getvalue().decode())
+        return output_values
 
-####waarde ophalen per E-mailID 19 (15-07-2014) voor functie Emailings-statistieken https://www.copernica.com/en/support/rest/emailing-statistics
+    def get_mailings(self, token):
+        self.token = token
+        params = {}
+        params['type'] = 'mass'  # Type te kiezen uit bulk of mass
+        params['limit'] = 1  # limiet standaard 9999, geen beperking
+        params['start'] = 0  # start altijd vanuit 0
+        params['access_token'] = token
+        return self.curl_options("https://api.copernica.com/emailings/?" + urllib.urlencode(params))
 
-
-params = {}
-emailing_ID = 19
-params['access_token'] = "5fad6ce4eeca28c7fbd41f16073dc2e4f67f823b925d40ea3fc6bd8eb313b5723fdc7a0af0d65a01c52fefeaa35beec951e3ff4fe905d0a12a1342f5b070a313"
-#toegangstoken, uniek per gebruiker. Deze is van E-mailmarketingbureau Ace and Tate
-
-http_header = [
-    'Content-Type: application/json'
-]
-
-url = "https://api.copernica.com/emailing/" + str(emailing_ID) + '/statistics/?' + urllib.urlencode(params)
-print(url)
-b = BytesIO()
-c = pycurl.Curl()
-
-c.setopt(pycurl.URL, url)
-c.setopt(pycurl.VERBOSE, 1)
-c.setopt(pycurl.WRITEDATA, b)
-
-c.perform()
-output_values = json.loads(b.getvalue().decode())
-print(type(output_values))
-print(output_values)
+    def get_mailing_details(self, token, emailing_id):
+        params = {}
+        params['access_token'] = token
+        return self.curl_options("https://api.copernica.com/emailing/" + str(emailing_id) + '/statistics/?' + urllib.urlencode(params))
 
 
+
+
+#Ophalen alle mailings ahv token van een klant
+print(Copernica().get_mailings("5fad6ce4eeca28c7fbd41f16073dc2e4f67f823b925d40ea3fc6bd8eb313b5723fdc7a0af0d65a01c52fefeaa35beec951e3ff4fe905d0a12a1342f5b070a313"))
+#ophalen statistieken ahv token van een klant
+print(Copernica().get_mailing_statistics("5fad6ce4eeca28c7fbd41f16073dc2e4f67f823b925d40ea3fc6bd8eb313b5723fdc7a0af0d65a01c52fefeaa35beec951e3ff4fe905d0a12a1342f5b070a313", "19"))
