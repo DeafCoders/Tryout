9245dbf88f4a7465dc39b960b86386bb0c3c9554
Mkandawire,Hugo H.H.C <h.mkandawire@student.fontys.nl>
2016-08-09 12:56:15 +0200

    Final commit of Learn REST

--- rest.py
+++ rest.py
@@ -7,11 +7,12 @@ except ImportError:
 # url = 'http://www.acme.com/products/3222'
 # response = urllib2.urlopen(url).read()
 
-url = 'http://www.acme.com/users/details'
+url = 'http://ip.jsontest.com'
 params = urllib.parse.urlencode({
   'firstName': 'John',
   'lastName': 'Doe'
 })
-binary_params = params.encode('encoding')
+binary_params = params.encode()
 
 response = urllib2.urlopen(url, binary_params).read()
+print(response)
